5dca820450815a08c85cad57651269b8c27c7b0c
roeehashai <roee.hashai@gmail.com>
2024-01-24 12:43:33 +0000

    loop correcrt

--- main.s
+++ main.s
@@ -38,12 +38,6 @@ main:
 
     # Initialize loop counter
     movb $0, %bl  # Counter register
-    // # Debugging output
-    // movzb %bl, %rsi
-    // movq $result_fmt, %rdi
-    // xorq %rax, %rax
-    // call printf
-    # Allocate space for the local variable (4 bytes assuming 32-bit integer)
     subq $16, %rsp
 
 .loop:
@@ -51,12 +45,6 @@ main:
     movq $user_guess_fmt, %rdi
     xorq %rax, %rax
     call printf
-    // # Debugging output
-    // movq %rcx, %rsi
-    // movq $result_fmt, %rdi
-    // xorq %rax, %rax
-    // call printf
-    # Read the guess input
     movq $scanf_fmt, %rdi
     movq %rsp, %rsi  # Load the address of the allocated space into rsi
     xorq %rax, %rax
@@ -65,12 +53,6 @@ main:
     # Increment loop counter
     incb %bl
 
-    // # Debugging output
-    // movb %bl, %sil
-    // movq $result_fmt, %rdi
-    // xorq %rax, %rax
-    // call printf
-    
     # Check if loop need to stop
     cmpb $5, %bl
     jl .loop
